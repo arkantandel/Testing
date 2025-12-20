5b772ad564d20dca5164d313ae7352d8c5b36c02
Arkan tandel <arkantandel@gmail.com>
2025-12-20 22:24:51 +0530

    Delete index.py

--- index.py
+++
@@ -1,6 +0,0 @@
-print("arkan tandel")
-print("wasim tandel")
-print ("fortune cloud is the average teaching acandemy")
-print("fortune cloud have the top student in the acaddemy")
-print("youtube is the best platform that can help you to learn the  things which you want")
-
